f1ec693f3cfcf48a5c648bc192430c749623725f
PtrMan <robertw89@googlemail.com>
2016-09-23 18:23:16 +0200

    first commit

---
+++ code/d/misced/lang/Line.d
@@ -0,0 +1,7 @@
+module misced.lang.Line;
+
+import misced.lang.Token : Token;
+
+class Line(EnumOperationType) {
+   public Token!EnumOperationType []Tokens;
+}
---
+++ code/d/misced/lang/ParserException.d
@@ -0,0 +1,7 @@
+module misced.lang.ParserException;
+
+class ParserException : Exception {
+	final this(string message) {
+		super("ParserException: " ~ message);
+	}
+}
