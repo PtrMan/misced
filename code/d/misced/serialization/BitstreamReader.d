80c157113cc6e94ca63fbf405f2cfb608eae9367
PtrMan <robertw89@googlemail.com>
2016-10-03 23:17:36 +0200

    added serialization into and from bitstreams

---
+++ code/d/misced/serialization/BitstreamReader.d
@@ -0,0 +1,287 @@
+module misced.serialization.BitstreamReader;
+
+/** \brief 
+ *
+ */
+class BitstreamReader(BitstreamSourceImplementation) {
+	protected final this() {
+	}
+
+	public final this(BitstreamSourceImplementation bitstreamSource) {
+		this.bitstreamSource = bitstreamSource;
+	}
+
+	/** \brief returns the next Bool
+	 *  \param successChained will be false if the function failed
+	 *  \return ...
+	 */
+	final public bool getBoolean(ref bool successChained) {
+		bool result;
+
+		if( !successChained ) {
+			return false;
+		}
+
+		if( isEndReached ) {
+			successChained = false;
+			return false;
+		}
+
+		// just to make sure no runtime error happens
+		if( !bitstreamSource.isValid ) {
+			successChained = false;
+			return false;
+		}
+
+		return bitstreamSource.readNextBit();
+	}
+
+	/** \brief returns a number of up to 16 bits wide
+	 *  \param successChained will be false if the function failed
+	 *  \return ...
+	 */
+	final public uint getUint_2__4_8_12_16(ref bool successChained) {
+		if( !successChained ) {
+			return 0;
+		}
+
+		if( isEndReached ) {
+			successChained = false;
+			return 0;
+		}
+
+		if( !bitstreamSource.isValid(2) ) {
+			successChained = false;
+			return 0;
+		}
+
+		uint numberOf4Bits = 0;
+		if( bitstreamSource.readNextBit() ) {
+			numberOf4Bits |= 2;
+		}
+
+		if( bitstreamSource.readNextBit() ) {
+			numberOf4Bits |= 1;
+		}
+
+		uint numberOfBits = (numberOf4Bits+1) * 4;
+
+		assert(numberOfBits > 0, "numberOfBits must be greater than 0!");
+
+		if( !bitstreamSource.isValid(numberOfBits) ) {
+			successChained = false;
+			return 0;
+		}
+
+
+		uint result = 0;
+		foreach( bitI; 0..numberOfBits ) {
+			if( bitstreamSource.readNextBit() ) {
+				result |= (1<<bitI);
+			}
+		}
+
+		return result;
+	}
+
+   // TOUNITTEST
+   // TODO< rename and use string >
+   /** \brief returns a fixed ength string with an 8 bit encoding
+    *
+    * \param numberOfSigns ...
+    * \param successChained ...
+    * \return ...
+    */
+    /+
+	final public char[] getString8FixedLength(uint numberOfSigns, ref bool successChained) {
+      char[] Return;
+      uint i;
+
+      if( !successChained )
+      {
+         return [];
+      }
+
+      for( i = 0; i < numberOfSigns; i++ )
+      {
+         Return ~= cast(char)getUint__n(8, successChained);
+      }
+
+      if( !successChained )
+      {
+         return [];
+      }
+
+      return Return;
+   }
+   +/
+
+	/** \brief returns a number of 'Bits' bits wide
+	 *  \param numberOfBits How many bits should be readed
+	 *  \param successChained will be false if the function failed
+	 *  \return ...
+	 */
+	final public uint getUint__n(uint numberOfBits, ref bool successChained) {
+		// only assert because it is in the gamecode very inpropable that something like this happens
+		assert(numberOfBits <= 32, "bits need to be less or equal to 32");
+
+		uint result = 0;
+
+		if( !successChained ) {
+			return 0;
+		}
+
+		if( numberOfBits == 0 ) {
+			successChained = false;
+			return 0;
+		}
+
+		if( !bitstreamSource.isValid(numberOfBits) ) {
+			successChained = false;
+			return 0;
+		}
+
+		foreach( bitI; 0..numberOfBits ) {
+			if( bitstreamSource.readNextBit() ) {
+				result |= (1 << bitI);
+			}
+		}
+
+		return result;
+	}
+
+   
+	/** \brief tries to return a fixed point value with n Bits
+	 * 
+	 *  \param bits How many bits does the value have (inclusive sign bit)
+	 *  \param Range Maximal range of this number
+	 *  \param successChained will be false if the function failed
+	 *  \return ...
+	 */
+	final public float getFixed__n(uint bits, float range, ref bool successChained) {
+		assert(bits > 2, "bits need to be bigger than 2");
+
+		// only assert because it is in the gamecode very inpropable that something like this happens
+		assert(bits <= 32, "bits need to be less or equal to 32");
+
+		assert(range > 0.0f, "range must be greater than 0.0");
+
+		if( !successChained ) {
+			return 0.0f;
+		}
+
+		bool sign = getBoolean(successChained);
+
+		if( !successChained ) {
+			return 0.0f;
+		}
+
+		int valueInt = getUint__n(bits-1, successChained);
+
+		if( !successChained ) {
+			return 0.0f;
+		}
+
+		if( sign ) {
+			valueInt *= -1;
+		}
+
+		return (cast(float)valueInt / cast(float)(1 << (bits-1))) * range;
+	}
+
+	// TODO< unit tests >
+	/** \brief tries to return 'Bits' bits
+	 * 
+	 *  \param bits How many bits should be read?
+	 *  \param successChained will be false if the function failed
+	 *  \return ...
+	 */
+	final public bool[] getBits(uint bits, ref bool successChained) {
+		bool[] result;
+
+		if( !successChained ) {
+			return result;
+		}
+
+		if( !bitstreamSource.isValid(bits)  ) {
+			successChained = false;
+
+			return result;
+		}
+
+		uint remaining = bits;
+
+		for(;;) {
+			if( remaining == 0 ) {
+				break;
+			}
+
+			result ~= bitstreamSource.readNextBit();
+
+			remaining--;
+		}
+
+
+		return result;
+	}
+
+
+	/** \brief returns a 32 bit float
+	 *  \param successChained will be false if the function failed
+	 *  \return ...
+	 */
+	final public float getFloat32(ref bool successChained) {
+		if( !successChained ) {
+			return 0.0f;
+		}
+
+		uint valueUint = getUint__n(32, successChained);
+
+		if( !successChained ) {
+			return 0.0f;
+		}
+
+		return *(cast(float*)&valueUint);
+	}
+
+   
+	/** \brief returns a half precision float value
+	 *
+	 * \param successChained will be false if the function failed
+	 * \return ...
+	 */
+	final public float getFloat16(ref bool successChained) {
+		if( !successChained ) {
+			return 0.0f;
+		}
+
+		uint valueUint = getUint__n(16, successChained);
+
+		if( !successChained ) {
+			return 0.0f;
+		}
+
+		// transfer sign bit
+		uint returnTemp = (valueUint & 0x8000) << (31-15);
+
+		// extract and transfer exponent
+		returnTemp |= (((valueUint >> 10) & 0x1f) << 26);
+
+		// extract and transfer mantise
+		returnTemp |= ((valueUint & 0x3ff) << 13);
+
+		return *(cast(float*)&returnTemp);
+	}
+
+	/** \brief is the end reached?
+	 *
+	 * \return ...
+	 */
+	final public @property bool isEndReached() {
+		// currently not implemented
+		// TODO?
+		return false;
+	}
+
+	protected BitstreamSourceImplementation bitstreamSource;
+}
---
+++ code/d/misced/serialization/BitstreamWriter.d
@@ -0,0 +1,272 @@
+module misced.serialization.BitstreamWriter;
+
+import std.math : abs;
+
+/** \brief 
+ *
+ */
+class BitstreamWriter(BitstreamDestinationImplementation) {
+	protected final this() {
+	}
+
+	public final this(BitstreamDestinationImplementation bitstreamDestination) {
+		this.bitstreamDestination = bitstreamDestination;
+	}
+
+	/** \brief add bits
+	 *
+	 * \param bits ...
+	 */
+	final public void addBits(bool[] bits) {
+		foreach(bit; bits) {
+			addBoolean(bit);
+		}
+	}
+
+	/** \brief Add a bool
+	 *
+	 * \param boolean ...
+	 */
+	final public void addBoolean(bool boolean) {
+		bitstreamDestination.addBoolean(boolean);
+	}
+
+	/** \brief Tries to add a Number with variable length to the Data
+	 *
+	 * Important is that the number must be smaller than 65536
+	 * \param value is the Number
+	 * \param successChained will be false if something went wrong
+	 */
+	final public void addUint_2__4_8_12_16(uint value, ref bool successChained) {
+		uint n;
+
+		if( !successChained ) {
+			return;
+		}
+
+		if( value & 0xFFFF0000 ) {
+			successChained = false;
+			return;
+		}
+		else if( value & 0xF000 ) {
+			n = 3;
+		}
+		else if( value & 0xF00 ) {
+			n = 2;
+		}
+		else if( value & 0xF0 ) {
+			n = 1;
+		}
+		else {
+			n = 0;
+		}
+
+		// add count
+		addBoolean((n>>1) & 1);
+		addBoolean(n & 1);
+
+		// add bits
+		uint bitCount = (n+1) * 4;
+
+		foreach( bitI; 0..bitCount ) {
+			addBoolean(((1 << bitI) & value) != 0);
+		}
+	}
+
+	/** \brief Tries to add an integer with n Bits
+	 *
+	 * Important is that the Number must be small enougth to fit into n bits
+	 *
+	 * \param value is the Number
+	 * \param bitCount Number of bits
+	 * \param SuccessChained will be false if something went wrong
+	 */
+	final public void addUint__n(uint value, uint bitCount, ref bool successChained) {
+		assert(bitCount > 0, "Bits need to be bigger than 0");
+
+		// only assert because it is in the gamecode very inpropable that something like this happens
+		assert(bitCount <= 32, "Bits need to be less or equal to 32");
+
+		if( !successChained ) {
+			return;
+		}
+
+		if( bitCount == 0 ) {
+			successChained = false;
+			return;
+		}
+
+		uint mask = 0;
+
+		for( uint bitI = 0; bitI < bitCount; bitI++ ) {
+			mask = mask | (1<<bitI);
+		}
+
+		// check if the Number fits into n bits
+		if( (~mask) & value ) {
+			successChained = false;
+			return;
+		}
+
+		foreach( bitI; 0..bitCount ) {
+			addBoolean(((1 << bitI) & value) != 0);
+		}
+	}
+
+	/** \brief Tries to add an fixed point value with n Bits
+	 *
+	 * Important is that the value must fit into range (checked)
+	 *
+	 * \param value is the Number
+	 * \param Bits Number of bits (inclusive sign)
+	 * \param range Maximal range of the number
+	 * \param successChained will be false if something went wrong
+	 */
+	final public void addFixed__n(float value, uint bits, float range, ref bool successChained) {
+		assert(bits > 2, "bits need to be bigger than 2");
+
+		// only assert because it is in the gamecode very inpropable that something like this happens
+		assert(bits <= 32, "bits need to be less or equal to 32");
+
+		assert(range > 0.0f, "range must be greater than 0.0");
+
+		if( !successChained ) {
+			return;
+		}
+
+		float valueTemp = abs(value);
+
+		// check if the number fits into range
+		if( valueTemp > range ) {
+			successChained = false;
+			return;
+		}
+
+		uint valueUint = cast(uint)( valueTemp/range * cast(float)(1 << (bits-1)) );
+
+		bool sign = valueTemp > 0.0f;
+		addBoolean(sign);
+		addUint__n(valueUint, bits-1, successChained);
+
+		// NOTE< successChained doesn't have to be checked! >
+	}
+
+	/** \brief Add a 16 bit float Value
+	 *
+	 * \param value ...
+	 */
+	final public void addFloat16(float value) {
+		bool successChained;
+
+		uint valueUint = *(cast(uint*)&value);
+
+		// transfer sign
+		uint converted = (valueUint & 0x80000000) >> 16;
+
+		// transfer exponent
+		converted |= (((valueUint >> 26) & 0x1f) << 10);
+
+		// transfer mantise
+		converted |= ((valueUint >> 13) & 0x3ff);
+
+		// works perfectly until here
+
+		successChained = true;
+
+		addUint__n(converted, 16, successChained);
+
+		assert(successChained, "Should not fail");
+	}
+
+	/** \brief Add a 32 bit float value
+	 *
+	 * \param value ...
+	 */
+	final public void addFloat32(float value) {
+		bool successChained;
+		
+		uint valueUint = *(cast(uint*)&value);
+
+		successChained = true;
+
+		addUint__n(valueUint, 32, successChained);
+
+		assert(successChained, "Should not fail");
+	}
+
+	final public void addArray__16(Type)(Type[] array, ref bool successChained) {
+		if( !successChained ) {
+			return;
+		}
+
+		if( array.length >= (1<<16) ) {
+			successChained = false;
+			return;
+		}
+
+		addUint__n(array.length, 16, successChained);
+
+		foreach( iterationElement; array ) {
+			addUint__n(iterationElement, Type.sizeof*8, successChained);
+		}
+	}
+
+	/+
+   // TODO UNITTEST
+   // TOUML
+   /** \brief Tries to add a string sended as 8 Bit ASCII codes
+    *
+    * The format which is written in the Bitstream is
+    *  * Length of the String (-1 if LengthGreaterThanZero)
+    *  * The String as 8 bit numbers
+    *
+    * \param String ...
+    * \param LengthBits how many bits should be used for the encoding of the length
+    * \param LengthGreaterThanZero if this is true it means that the Length in the bitstream is one less than the real length, this can safe a bit if a string can'tbe empty
+    * \param successChained will be false if something went wrong
+    */
+   final public void addWideString8(WideString String, uint LengthBits, bool LengthGreaterThanZero, ref bool successChained)
+   {
+      uint StringLength;
+
+      if( !successChained )
+      {
+         return;
+      }
+
+      StringLength = String.getLength();
+
+      if( LengthGreaterThanZero && StringLength == 0 )
+      {
+         successChained = false;
+         return;
+      }
+
+      if( LengthBits > 32 )
+      {
+         successChained = false;
+         return;
+      }
+
+      if( LengthGreaterThanZero )
+      {
+         this.addUint__n(StringLength-1, LengthBits, successChained);
+      }
+      else
+      {
+         this.addUint__n(StringLength, LengthBits, successChained);
+      }
+
+      if( !successChained )
+      {
+         return;
+      }
+
+      foreach( Sign; String.getSigns )
+      {
+         this.addUint__n(Sign, 8, successChained);
+      }
+   }+/
+
+	protected BitstreamDestinationImplementation bitstreamDestination;
+}
